1c076bf9798213bc3cec9cf0dec3c33a7119fe11
Sumayya-Afrin <sumayyafrin.mohammad@proclink.com>
2024-08-18 13:33:45 +0530

    created user entity and done with signup, login

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import cors from "cors";
 import craftsRouter from "./routes/crafts.route.js";
+import usersRouter from "./routes/users.route.js";
 import express from "express";
 const app = express();
 
@@ -12,6 +13,7 @@ app.get("/", function (request, response) {
 });
 
 app.use("/Crafts", craftsRouter);
+app.use("/users", usersRouter);
 
 app.listen(PORT, () => console.log(`The server started in: ${PORT} ✨✨`));
 
